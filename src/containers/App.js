748fbfb94aee45570649e41cbd3f11447c88e216
DanL12186 <danlrcn@hotmail.com>
2017-08-18 13:11:11 -0400

    Imported connect, bindactioncreators, and my components + UIkit
    into app.js. added BrowserRouter wrapping. Changed state to props.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,32 +1,38 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
 import logo from '../logo.svg';
 import './App.css';
 import Movies from './Movies'
+import AddMovie from './AddMovie'
+import TopMovies from './TopMovies'
+import Home from './Home'
+import Navbar from '../components/navbar'
+import uikit from '../uikit.css'
 
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 //import {addMovies, deleteMovies} from './reducers/movies'
 
 class App extends Component {
   constructor(props) {
   super(props)
-
-  this.state = {
-    movies: [],
-  }
 }
 
   componentDidMount() {
     fetch('http://localhost:3001/api/movies')// || fetch ("localhost:3000/api/movies")
       .then(response => response.json())
-      .then(movies => this.setState({movies}))
+      .then(movies => this.props.movies(movies))
   }
 
   render() {
     return (
-      <div className="App">
-       <p><img src={logo} className="App-logo" height="42" alt="My logo"/></p>
-        App Container
-        <Movies movies = {this.state.movies} />
-      </div>
+
+      <BrowserRouter>
+       <div className="App">
+         <Navbar />
+         <p><img src={logo} className="App-logo" height="42" alt="My logo"/></p>
+       </div>
+     </BrowserRouter>
     );
   }
 }
