7f497d21e19844862811693755bd48992d1bf791
DanL12186 <danlrcn@hotmail.com>
2017-09-02 18:12:34 -0400

    Replaced add rating w/update rating.. creation adds rating.

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -2,10 +2,12 @@ export default function manageMovieRatings(state = {
     movies: []
   }, action) {
   switch (action.type) {
-    case 'ADD_RATING':
-      // const rating = Object.assign({}, action.movie, {id: id});
-      // // return { movies: state.movies.concat(restaurant) }
-     case 'ADD_MOVIES':
+    case 'UPDATE_MOVIE_RATING':
+      const filterOutCurrentMovie = state.movies.filter(m => m.id !== action.movie.id)
+    return Object.assign({}, state, {
+      movies: filterOutCurrentMovie.concat(action.movie)
+     });
+    case 'ADD_MOVIES':
      return Object.assign({}, state, {
         movies: action.movies
       });
