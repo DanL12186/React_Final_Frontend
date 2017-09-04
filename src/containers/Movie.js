d9c51325d0ef32fcec8d1d726050b557659a5f0e
DanL12186 <danlrcn@hotmail.com>
2017-09-04 17:30:40 -0400

    updated movie component

--- src/containers/Movie.js
+++ src/containers/Movie.js
@@ -1,41 +1,47 @@
 import React from 'react'
-const clickMe = (movie, rating) => {
-  //console.log(movie)
-  console.log(rating)
 
-    // fetch(`https://www.google.com/search?q=${movie.title}+%28Movie%29`)
-    //   .then(response=> console.log(response))
-//        function(imageSearch) {
-//     const image = $("#uid_0") || $("#uid_2")
-//     charDiv.append(`<img>${imageSearch.image}</img>`)
-// })
+const vote = (movie, input, updateVote) => {
+  let rating = ((movie.rating * movie.votes) + input)/(movie.votes + 1)
+
+  const updatedMovie = movie;
+  updatedMovie.rating = rating;
+  updatedMovie.votes++;
+
+  rating = rating.toString().replace(".","_")
+
+  fetch(`http://localhost:3001/api/movies/${movie.id}/votes/${rating}`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    }).then (response => {
+      if (response.status === 204) {
+        updateVote(updatedMovie)
+      }
+    })
+   }
+
+const roundRatings = (rating) => {
+  return Math.round(rating * 100) / 100
 }
 
-
-// function vote(n) { //running average
-//   votes++;
-//   return rating += ((n - rating)/votes);
-// }
-
-
-const Movie = ({movie}) => (
+const Movie = (props) => (
   <div>
-    <h3>{movie.title}</h3>
-    <p>Year: {movie.year}</p>
-    <p>User Rating: {movie.rating} Stars</p>
-    <p>MPAA Rating: {movie.mpaa}</p>
-    <p>Number of Votes: {movie.votes}</p>
+    <br/>
+    <h3>{props.movie.title}</h3>
+    <p>Year: {props.movie.year}</p>
+    <p>User Rating: {roundRatings(props.movie.rating)} Stars</p>
+    <p>MPAA Rating: {props.movie.mpaa}</p>
+    <p>Number of Votes: {props.movie.votes}</p>
 
     <h4>Rate Me</h4>
-    <button onClick={() => clickMe(movie, 1)}>1</button>
-    <button onClick={() => clickMe(movie, 2)}>2</button>
-    <button onClick={() => clickMe(movie, 3)}>3</button>
-    <button onClick={() => clickMe(movie, 4)}>4</button>
-    <button onClick={() => clickMe(movie, 5)}>5</button>
-    <br/>
-
+    <button onClick={() => vote(props.movie, 1, props.updateVote)}>1</button>
+    <button onClick={() => vote(props.movie, 2, props.updateVote)}>2</button>
+    <button onClick={() => vote(props.movie, 3, props.updateVote)}>3</button>
+    <button onClick={() => vote(props.movie, 4, props.updateVote)}>4</button>
+    <button onClick={() => vote(props.movie, 5, props.updateVote)}>5</button>
   </div>
-
 )
 
 export default Movie
