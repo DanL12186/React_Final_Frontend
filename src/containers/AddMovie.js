a2a21435c22fe293c034e8fdfdbc3345df976330
DanL12186 <danlrcn@hotmail.com>
2017-09-04 17:29:02 -0400

    pushing functional buttons up to github

--- src/containers/AddMovie.js
+++ src/containers/AddMovie.js
@@ -6,12 +6,61 @@ class AddMovie extends Component {
 
     this.state = {
       movies: [],
+      title: '',
+      year: '',
+      mpaa: '',
+      rating: '',
     }
+  }
+
+  handleSubmit = (event) => {
+    fetch("http://localhost:3001/api/movies", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state)
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
   render() {
     return (
-      <div>Add Movie</div>
+      <div>
+        <h2>Add Movie</h2>
+         <br/>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <div>
+            <h4>Title</h4>
+            <input type="text" name="title" onChange={(event) => this.handleChange(event)}/>
+          </div>
+           <br/>
+          <div>
+            <h4>Year</h4>
+            <input type="text" name="year" onChange={(event) => this.handleChange(event)}/>
+          </div>
+            <br/>
+          <div>
+            <h4>MPAA Rating</h4>
+            <input type="text" name="mpaa" onChange={(event) => this.handleChange(event)}/>
+          </div>
+           <br/>
+          <div>
+            <h4>User Rating</h4>
+            <input type="text" name="rating" onChange={(event) => this.handleChange(event)}/>
+          </div>
+          <br/>
+          <input type="submit" value="Add movie"/>
+        </form>
+      </div>
     )
   }
 }
+
+export default AddMovie;
