6dba8d5d835b260c73072a80a0da987375202ab4
DanL12186 <danlrcn@hotmail.com>
2018-02-03 18:41:19 -0500

    preventDefault; likes, roundRatings

--- src/containers/AddMovie.js
+++ src/containers/AddMovie.js
@@ -14,6 +14,7 @@ class AddMovie extends Component {
   }
 
   handleSubmit = (event) => {
+    event.preventDefault()
     const { history } = this.props
     fetch("http://localhost:3001/api/movies", {
       method: 'POST',
